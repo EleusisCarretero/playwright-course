0fb1c832a42cc486b93f17ac7ce87dfd2543ce81
Eleusis Carretero <eleusis.carrtero@gmail.com>
2025-02-21 23:49:52 -0600

    new page for visits

---
+++ page-objects/visitPage.js
@@ -0,0 +1,26 @@
+import { BaseProduct } from "./BasePage";
+
+export class VisitPage extends BaseProduct{
+    constructor(page){
+        super(page)
+        this.mobileNavigation = this.page.locator("[data-qa='mobile-navication']");
+        this.checkout = this.page.getByRole("link", {name: "Checkout"});
+        this.__basketCounter__ = this.page.locator("[data-qa='header-basket-count']");
+        
+
+    }
+
+    goToCheckout = async () => {
+        await this.checkout.waitFor();
+        await this.checkout.click();
+        await this.page.waitForTimeout(1000);
+        await this.page.waitForURL("/basket")
+    }
+
+    get basketCounter() {
+        return (async () => {
+            // await this.__basketCounter__.waitFor();
+            return await this.__basketCounter__.textContent();
+        })();
+    }
+}
