e619c4166eb609d852cfae30bab6c8582bf2387c
Eleusis Carretero <eleusis.carrtero@gmail.com>
2025-02-25 14:37:31 -0600

    new test for mock request

---
+++ tests/test_my_account_mockspec.js
@@ -0,0 +1,23 @@
+import { test, expect} from "@playwright/test"
+import {MyAcountPage} from "../page-objects/myAcountPage"
+import { adminDetails } from "./../data/userDetails.js"
+
+test.only("Testing my acount using mock netwotking request", async ({page})=>{
+    const myAccountPage =  new MyAcountPage(page);
+    const loginToken = await myAccountPage.getLoginToken(adminDetails.username, adminDetails.password);
+    await page.route("**/api/user**", async (route, request) => {
+        await route.fulfill({
+            status: 500,
+            contentType: "application/json",
+            body: JSON.stringify({message: "PLAYWRIGHT ERROR FROM MOCKING"}),
+        })
+    })
+    await myAccountPage.openPage();
+    await page.evaluate(([loginTokenInsideBrowserCode]) => {
+        document.cookie = "token=" + loginTokenInsideBrowserCode
+    }, [loginToken])
+    await myAccountPage.openPage();
+    await myAccountPage.waitForPageHeading();
+    await myAccountPage.waitForErrorMessage();
+
+});
